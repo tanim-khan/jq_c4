13af54745ad095932d705655f63ed96f8868936c
tanim-khan <54594757+tanim-khan@users.noreply.github.com>
2019-12-11 18:12:02 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+// counter start
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+// counter end
+// wow start
+ new WOW().init();
+// wow end
+
+
+})
+
+
+
+
